44f13b060cb59ee15cccfe0fece69698491d622c
Prajwal-061 <mainaliprajwal123@gmail.com>
2025-12-02 17:35:32 +0530

    Added multiple path parameter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 import json
 
 app=FastAPI()
@@ -24,3 +24,37 @@ def view():
     
     return data
 
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id:str):
+    data=load_data()
+    
+    if patient_id in data:
+        return data[patient_id]
+    return {"message":"Patient not found"}
+
+
+@app.get("/patient/city/{patient_city}")
+def view_patient_city(patient_city:str):
+    data=load_data()
+   
+    result={}
+    for pid,info in data.items():
+     if info["city"].lower()==patient_city.lower():
+        result[pid]=info
+    if not result:
+        return {"message":"NO patients found in this city"}
+    
+    return result
+
+@app.get("/patient/gender/{patient_gender}")
+def view_patient_gender(patient_gender:str =Path(...,description="Gender the petient to get",example="male")):
+    data=load_data()
+   
+    result={}
+    for pid,info in data.items():
+     if info["gender"].lower()==patient_gender.lower():
+        result[pid]=info
+    if not result:
+        return {"message":"NO patients found of this gender"}
+    
+    return result
